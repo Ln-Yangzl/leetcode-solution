45cccd130b4cf3997f6c45ab699ecdc2feeb4a9d
zlyang <565213263@qq.com>
2021-03-19 18:13:52 +0800

    start 17

---
+++ 71. Simplify Path.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <list>
+#include <fstream>
+
+using namespace std;
+
+string simplifyPath(string path) {
+    list<string> record;
+    record.push_back("$");        
+}
